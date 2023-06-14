ab00011c8ce3847a3c6e5b680d48e7ef113d792b
Jozsef Vass <jozsef@discordapp.com>
2023-06-14 16:02:27 -0700

    bring over armcrypto from master branch

--- src/libsodium/sodium/runtime.c
+++ src/libsodium/sodium/runtime.c
@@ -11,6 +11,7 @@ typedef struct CPUFeatures_ {
     int initialized;
     int has_neon;
     int has_sse2;
+    int has_armcrypto;
     int has_sse3;
     int has_ssse3;
     int has_sse41;
@@ -48,25 +49,92 @@ static CPUFeatures _cpu_features;
 static int
 _sodium_runtime_arm_cpu_features(CPUFeatures * const cpu_features)
 {
-#ifndef __arm__
     cpu_features->has_neon = 0;
-    return -1;
-#else
-# ifdef __APPLE__
-#  ifdef __ARM_NEON__
+    cpu_features->has_armcrypto = 0;
+
+#ifndef __ARM_ARCH
+    return -1; /* LCOV_EXCL_LINE */
+#endif
+
+#if defined(__ARM_NEON) || defined(__aarch64__)
     cpu_features->has_neon = 1;
-#  else
-    cpu_features->has_neon = 0;
-#  endif
-# elif defined(HAVE_ANDROID_GETCPUFEATURES) && \
-    defined(ANDROID_CPU_ARM_FEATURE_NEON)
+#elif defined(HAVE_ANDROID_GETCPUFEATURES) && defined(ANDROID_CPU_ARM_FEATURE_NEON)
     cpu_features->has_neon =
         (android_getCpuFeatures() & ANDROID_CPU_ARM_FEATURE_NEON) != 0x0;
-# else
-    cpu_features->has_neon = 0;
+#elif defined(__aarch64__) && defined(AT_HWCAP)
+# ifdef HAVE_GETAUXVAL
+    cpu_features->has_neon = (getauxval(AT_HWCAP) & (1L << 1)) != 0;
+# elif defined(HAVE_ELF_AUX_INFO)
+    {
+        unsigned long buf;
+        if (elf_aux_info(AT_HWCAP, (void *) &buf, (int) sizeof buf) == 0) {
+            cpu_features->has_neon = (buf & (1L << 1)) != 0;
+        }
+    }
 # endif
-    return 0;
+#elif defined(__arm__) && defined(AT_HWCAP)
+# ifdef HAVE_GETAUXVAL
+    cpu_features->has_neon = (getauxval(AT_HWCAP) & (1L << 12)) != 0;
+# elif defined(HAVE_ELF_AUX_INFO)
+    {
+        unsigned long buf;
+        if (elf_aux_info(AT_HWCAP, (void *) &buf, (int) sizeof buf) == 0) {
+            cpu_features->has_neon = (buf & (1L << 12)) != 0;
+        }
+    }
+# endif
 #endif
+
+    if (cpu_features->has_neon == 0) {
+        return 0;
+    }
+
+#if __ARM_FEATURE_CRYPTO
+    cpu_features->has_armcrypto = 1;
+#elif defined(__APPLE__) && defined(CPU_TYPE_ARM64) && defined(CPU_SUBTYPE_ARM64E)
+    {
+        cpu_type_t    cpu_type;
+        cpu_subtype_t cpu_subtype;
+        size_t        cpu_type_len = sizeof cpu_type;
+        size_t        cpu_subtype_len = sizeof cpu_subtype;
+
+        if (sysctlbyname("hw.cputype", &cpu_type, &cpu_type_len,
+                         NULL, 0) == 0 && cpu_type == CPU_TYPE_ARM64 &&
+            sysctlbyname("hw.cpusubtype", &cpu_subtype, &cpu_subtype_len,
+                         NULL, 0) == 0 &&
+            (cpu_subtype == CPU_SUBTYPE_ARM64E ||
+                cpu_subtype == CPU_SUBTYPE_ARM64_V8)) {
+            cpu_features->has_armcrypto = 1;
+        }
+    }
+#elif defined(HAVE_ANDROID_GETCPUFEATURES) && defined(ANDROID_CPU_ARM_FEATURE_AES)
+    cpu_features->has_armcrypto =
+        (android_getCpuFeatures() & ANDROID_CPU_ARM_FEATURE_AES) != 0x0;
+#elif defined(__aarch64__) && defined(AT_HWCAP)
+# ifdef HAVE_GETAUXVAL
+    cpu_features->has_armcrypto = (getauxval(AT_HWCAP) & (1L << 3)) != 0;
+# elif defined(HAVE_ELF_AUX_INFO)
+    {
+        unsigned long buf;
+        if (elf_aux_info(AT_HWCAP, (void *) &buf, (int) sizeof buf) == 0) {
+            cpu_features->has_armcrypto = (buf & (1L << 3)) != 0;
+        }
+    }
+# endif
+#elif defined(__arm__) && defined(AT_HWCAP2)
+# ifdef HAVE_GETAUXVAL
+    cpu_features->has_armcrypto = (getauxval(AT_HWCAP2) & (1L << 0)) != 0;
+# elif defined(HAVE_ELF_AUX_INFO)
+    {
+        unsigned long buf;
+        if (elf_aux_info(AT_HWCAP2, (void *) &buf, (int) sizeof buf) == 0) {
+            cpu_features->has_armcrypto = (buf & (1L << 0)) != 0;
+        }
+    }
+# endif
+#endif
+
+    return 0;
 }
 
 static void
@@ -246,6 +314,12 @@ int
 sodium_runtime_has_neon(void)
 {
     return _cpu_features.has_neon;
+}
+
+int
+sodium_runtime_has_armcrypto(void)
+{
+    return _cpu_features.has_armcrypto;
 }
 
 int
